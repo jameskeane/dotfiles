f7df38cd23d87efe1c98f97a07376015f1cd7c97
James Keane <james.keane@gmail.com>
2015-07-10 23:17:44 -0400

    Fed up with ibus, also add xmonad minimize plugin

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,13 +5,22 @@ import XMonad.Actions.PhysicalScreens
 import XMonad.Util.EZConfig
 import XMonad.Util.Paste
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Minimize
 
 myModMask = mod1Mask
 myFloatMask = composeAll [
-	className =? "qemu" --> doFloat
+	className =? "qemu" --> doFloat,
+        className =? "Zenity" --> doFloat
 	]
 
 myKeys =
+  -- Set up keys for minimizing windows (mostly for steam)
+  [
+    ((myModMask, xK_m ), withFocused minimizeWindow),
+    ((myModMask .|. shiftMask, xK_m ), sendMessage RestoreNextMinimizedWin)
+  ]
+
+  ++
   -- Use Custom Screen Maps 
   [((myModMask .|. mask, key), f sc)
       | (key, sc) <- zip [xK_w, xK_e, xK_r] [1, 2, 0]
@@ -23,6 +32,8 @@ main =
 	manageHook = myFloatMask <+> manageHook gnomeConfig, 
 	modMask = myModMask,
         logHook = updatePointer $ Relative 0.5 0.5,
-	layoutHook = smartBorders (layoutHook gnomeConfig)
+	layoutHook = minimize $ smartBorders (layoutHook gnomeConfig),
+        focusedBorderColor = "#E84C3D",
+        normalBorderColor = "#595959"
     }
     `additionalKeys` myKeys
